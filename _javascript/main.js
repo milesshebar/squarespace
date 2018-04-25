117434de4978be3ae7409891dd65e1172e3950b8
Emma Hood <hoode@kenyon.edu>
2018-04-24 22:59:39 -0400

    Fixed js file

--- _javascript/main.js
+++ _javascript/main.js
@@ -10,7 +10,7 @@ d3.selectAll('tile is-child notification is-primary').on('click', changeColor);
 function changeColor() {
     if ($(this).hasClass('is-primary')) {
         $(this).removeClass('is-primary').addClass('is-secondary');
-    } else if ($(this).hasClass('is-secondary'))
+    } else if ($(this).hasClass('is-secondary')) {
         $(this).removeClass('is-secondary').addClass('is-primary');
     }
 }
