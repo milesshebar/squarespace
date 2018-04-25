047c8f72a7b31acf86c3e512c281e8343e710fe3
Emma Hood <hoode@kenyon.edu>
2018-04-24 23:04:56 -0400

    Fixed more js

--- _javascript/main.js
+++ _javascript/main.js
@@ -5,12 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
 
-d3.selectAll('tile is-child notification is-primary').on('click', changeColor);
+$(document).ready(function() {
+    d3.selectAll('tile is-child notification is-primary').on('click', changeColor);
+});
 
 function changeColor() {
-    if ($(this).hasClass('is-primary')) {
-        $(this).removeClass('is-primary').addClass('is-secondary');
-    } else if ($(this).hasClass('is-secondary')) {
-        $(this).removeClass('is-secondary').addClass('is-primary');
+    if (d3.select(this).hasClass('is-primary')) {
+        d3.select(this).removeClass('is-primary').addClass('is-secondary');
+    } else if (d3.select(this).hasClass('is-secondary')) {
+        d3.select(this).removeClass('is-secondary').addClass('is-primary');
     }
 }
