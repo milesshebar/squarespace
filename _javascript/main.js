d36dc85c2d8b7da6123ba7201f3eb448c10f3259
Emma Hood <hoode@kenyon.edu>
2018-04-27 09:51:20 -0400

    Added receiving function for initializing colors

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,20 +1,18 @@
 import {select, selectAll} from "d3-selection";
 
+var s=io();
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
 
-$(document).ready(function() {
-    d3.selectAll('tile is-child notification is-primary').on('click', changeColor);
+s.on('message', function(message) {
+    //message: {id: 'tx', colorname: 'is-color'}
+    //d3.select(message.id).removeClass("current colorclass").addClass(message.colorname);
 });
 
+//d3.selectAll('tile is-child notification').on('click', changeColor);
+
 function changeColor() {
-    if ($(this).hasClass('is-primary')) {
-        console.log('Changed Primary Color')
-        $(this).removeClass('is-primary').addClass('is-secondary');
-    } else if ($(this).hasClass('is-secondary')) {
-        console.log('Changed Secondary Color')
-        $(this).removeClass('is-secondary').addClass('is-primary');
-    }
+
 }
