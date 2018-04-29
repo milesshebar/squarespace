b2e778510789fae3935b66056c2d7708c4832edf
Emma Hood <hoode@kenyon.edu>
2018-04-29 15:58:19 -0400

    Changed d3 select lines to jquery reference

--- _javascript/main.js
+++ _javascript/main.js
@@ -14,17 +14,17 @@ s.on('message', function(message) {
     //message: {id: '#tx', colorname: 'is-color'}
     console.log(message.id);
     if(message.colorname == 'is-primary') {
-      d3.select(message.id).removeClass('is-danger').addClass(message.colorname);
+      $(message.id).removeClass('is-danger').addClass(message.colorname);
     } else if(message.colorname == 'is-link') {
-      d3.select(message.id).removeClass('is-primary').addClass(message.colorname);
+      $(message.id).removeClass('is-primary').addClass(message.colorname);
     } else if(message.colorname == 'is-info') {
-      d3.select(message.id).removeClass('is-link').addClass(message.colorname);
+      $(message.id).removeClass('is-link').addClass(message.colorname);
     } else if(message.colorname == 'is-success') {
-      d3.select(message.id).removeClass('is-info').addClass(message.colorname);
+      $(message.id).removeClass('is-info').addClass(message.colorname);
     } else if(message.colorname == 'is-warning') {
-      d3.select(message.id).removeClass('is-success').addClass(message.colorname);
+      $(message.id).removeClass('is-success').addClass(message.colorname);
     } else if(message.colorname == 'is-danger') {
-      d3.select(message.id).removeClass('is-warning').addClass(message.colorname);
+      $(message.id).removeClass('is-warning').addClass(message.colorname);
     } else {
       console.log('Error loading color class');
     }
