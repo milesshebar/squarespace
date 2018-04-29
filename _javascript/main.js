e7fac65eb7943a02f932d256f5a0527928c4bf30
Emma Hood <hoode@kenyon.edu>
2018-04-29 14:23:01 -0400

    Functionality to push id to server and change tile from message

--- _javascript/main.js
+++ _javascript/main.js
@@ -4,15 +4,27 @@ var s=io();
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
+  d3.selectAll('tile is-child notification').on('click', function() {
+    s.emit('message', {id: this.id});
+  });
 });
 
 s.on('message', function(message) {
-    //message: {id: 'tx', colorname: 'is-color'}
-    //d3.select(message.id).removeClass("current colorclass").addClass(message.colorname);
+    //message: {id: '#tx', colorname: 'is-color'}
+    console.log(message.id);
+    if(message.colorname == 'is-primary') {
+        d3.select(message.id).removeClass('is-danger').addClass(message.colorname);
+    } else if(message.colorname == 'is-link') {
+      d3.select(message.id).removeClass('is-primary').addClass(message.colorname);
+    } else if(message.colorname == 'is-info') {
+      d3.select(message.id).removeClass('is-link').addClass(message.colorname);
+    } else if(message.colorname == 'is-sucess') {
+      d3.select(message.id).removeClass('is-info').addClass(message.colorname);
+    } else if(message.colorname == 'is-warning') {
+      d3.select(message.id).removeClass('is-success').addClass(message.colorname);
+    } else if(message.colorname == 'is-danger') {
+      d3.select(message.id).removeClass('is-warning').addClass(message.colorname);
+    } else {
+      console,log('Error loading color class');
+    }
 });
-
-//d3.selectAll('tile is-child notification').on('click', changeColor);
-
-function changeColor() {
-
-}
