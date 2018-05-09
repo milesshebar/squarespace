7d0672e50cc6cf6693b0bf8ef07130fb36966b70
Miles Shebar <miles.shebar@gmail.com>
2018-05-09 17:10:34 -0400

    beautify code and add comments

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,6 +1,7 @@
 var s = io.connect('http://localhost:8080');
 //change this link to cslab.kenyon.edu:9000 to run on cslab
 
+//Function that sends all client color changes to server
 document.addEventListener('DOMContentLoaded', () => {
   d3.selectAll('.tile.is-child').on('click', function() {
     console.log(this.id + ' ID clicked');
@@ -8,21 +9,22 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+//Function that recieves new color changes from the server
 s.on('message', function(message) {
-    //message: {id: '#tx', colorname: 'is-color'}
-    if(message.colorname == 'is-primary') {
-      $(message.id).removeClass('is-danger').addClass(message.colorname);
-    } else if(message.colorname == 'is-link') {
-      $(message.id).removeClass('is-primary').addClass(message.colorname);
-    } else if(message.colorname == 'is-info') {
-      $(message.id).removeClass('is-link').addClass(message.colorname);
-    } else if(message.colorname == 'is-success') {
-      $(message.id).removeClass('is-info').addClass(message.colorname);
-    } else if(message.colorname == 'is-warning') {
-      $(message.id).removeClass('is-success').addClass(message.colorname);
-    } else if(message.colorname == 'is-danger') {
-      $(message.id).removeClass('is-warning').addClass(message.colorname);
-    } else {
-      console.log('Error loading color class');
-    }
+  //object provided: "message: {id: '#tx', colorname: 'is-color'}"
+  if (message.colorname == 'is-primary') {
+    $(message.id).removeClass('is-danger').addClass(message.colorname);
+  } else if (message.colorname == 'is-link') {
+    $(message.id).removeClass('is-primary').addClass(message.colorname);
+  } else if (message.colorname == 'is-info') {
+    $(message.id).removeClass('is-link').addClass(message.colorname);
+  } else if (message.colorname == 'is-success') {
+    $(message.id).removeClass('is-info').addClass(message.colorname);
+  } else if (message.colorname == 'is-warning') {
+    $(message.id).removeClass('is-success').addClass(message.colorname);
+  } else if (message.colorname == 'is-danger') {
+    $(message.id).removeClass('is-warning').addClass(message.colorname);
+  } else {
+    console.log('Error loading color class');
+  }
 });
